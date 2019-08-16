489aa1fee4f1c04d84f67db597de22faadfadf29
wucl <1445860318@qq.com>
2019-08-16 21:27:20 +0800

    创建mock服务

---
+++ server.js
@@ -0,0 +1,9 @@
+const restify = require('restify');
+const server = restify.createServer();
+ 
+server.get('/hello', (req, res, next) => {
+ res.send('Hello World');
+ return next();
+});
+ 
+server.listen(8000, () => console.log('%s listening at %s', server.name, server.url));
